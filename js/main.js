332bd3453310556d1412215b2233976657e11feb
Khasan077 <alikulovkh7@gmail.com>
2023-11-28 01:39:19 +0500

    toggle dark-theme on site header them togler click

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+const elSiteThemeToggler = document.querySelector('.site-header-theme-toggler');
+elSiteThemeToggler.addEventListener('click', function name(params) {
+    document.body.classList.toggle('dark-mode'); 
+});
+
+
+
